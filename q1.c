9cf57f1032662bd05534098f62207b16373879e3
PulakIIIT <jimrohn28@gmail.com>
2020-08-15 22:18:04 +0530

    bug fixes

--- q1.c
+++ q1.c
@@ -7,11 +7,9 @@
 
 signed main(int argc, char *argv[])
 {
-    printf("start\n");
     char buff[2];
     char* pathname = argv[1];
     char* filename = pathname;
-    printf("%s\n", pathname);
     struct stat sb;
     int file_desc_end = open(pathname,  O_RDWR);
     stat(pathname, &sb);
@@ -19,12 +17,11 @@ signed main(int argc, char *argv[])
     char new_file[1000];
     strcpy(new_file, "./Assignment/");
     strcat(new_file, filename);
-    printf("size is %lld\n", sb.st_size);
     int file_desc_start = open(new_file,  O_WRONLY |O_CREAT, 0777 );
     int curr_location = sb.st_size - 1;
     int total_bytes = sb.st_size;
     int bytes_read = 0;
-    int done_percent = 0;
+    int done_percent = -1;
     while(bytes_read < total_bytes){
         lseek(file_desc_start, bytes_read, SEEK_SET);
         lseek(file_desc_end, curr_location, SEEK_SET);
@@ -36,13 +33,14 @@ signed main(int argc, char *argv[])
         if(done_percent_new > done_percent)
         {
             fflush(stdout);
-            printf("%d\r", done_percent_new);
+            char message[100];
+            int size = sprintf(message, "Progress = %d%%\r", done_percent_new);
+            write(1, message, size);
             done_percent = done_percent_new;
 
         }
     }
     int close(int fd);
-    printf("%s", new_file);
     return 0;
 }
 
--- s.sh
+++ s.sh
@@ -1,2 +1,2 @@
 rm -r Assignment
-gcc main.c && ./a.out "enwik8"
+gcc q1.c && ./a.out "enwik8"
