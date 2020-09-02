78024688f658b5cc4e7e681de2b42332e887b0dc
PulakIIIT <jimrohn28@gmail.com>
2020-09-03 00:24:15 +0530

    changing terminal modes no life

---
+++ Assignment2/Src/nightswatch.c
@@ -0,0 +1,5 @@
+//
+// Created by Pulak Malhotra on 02/09/20.
+//
+
+#include "nightswatch.h"
---
+++ Assignment2/Src/nightswatch.h
@@ -0,0 +1,8 @@
+//
+// Created by Pulak Malhotra on 02/09/20.
+//
+
+#ifndef UNTITLED_NIGHTSWATCH_H
+#define UNTITLED_NIGHTSWATCH_H
+
+#endif //UNTITLED_NIGHTSWATCH_H
